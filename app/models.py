91633677ca4d19db5e44937d86a6d3e4702dfa7a
nikokvirtia1984 <kvirtian@yahoo.com>
2025-03-11 21:23:10 +0400

    finish model file

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from app.database import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 class User(Base):
     __tablename__ = 'users'
@@ -8,3 +9,37 @@ class User(Base):
     heshed_password = Column(String)
     role = Column(String)
     
+    courses = relationship("Course", back_populates="instructor")
+    enrollment = relationship('Enrollment', secondary='users')
+
+
+class lesson(Base):
+    __tablename__ = "lessons"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    content = Column(String)
+    course_id = Column(Integer, ForeignKey('courses.id'))
+    
+    course = relationship('Course', back_populates='lessons')
+    
+class Course(Base):
+    __tablename__ = 'courses'
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    duration = Column(Integer)
+    instructor_id = Column(Integer, ForeignKey('users.id'))
+    
+    instructor = relationship('User', back_populates='courses')
+    lessons = relationship('Lessons', back_populates='course')
+    enrollments = relationship('Enrollment', back_populates='course') 
+    
+class Enrollment(Base):
+    __tablename__="enrollment"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    course_id = Column(Integer, ForeignKey('courses.id'))
+    progress = Column(Integer)
+    
+    users = relationship('User', back_populates='enrollment')
+    course = relationship('Course', back_populates='enrollment')
